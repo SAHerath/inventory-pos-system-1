559a28c57f331bb3961006b4f9448b2d74ef79ec
unknown <h.supun.anuradha@gmail.com>
2024-04-03 16:58:14 +0530

    fix side navigation drop menu bug when collaps by adding menu-item, menu-icon, menu-drop, drop-icon classes

--- app/views/includes/navigation.php
+++ app/views/includes/navigation.php
@@ -1,7 +1,7 @@
 <header class="header">
   <nav>
     <!-- left navigation -->
-    <a class="toggle-lg" role="button" onclick="dashbC.toggleMenu()">
+    <a class="toggle-lg" role="button" onclick="dashbCon.toggleMenu()">
       <i class="fas fa-bars"></i>
     </a>
     <a class="" href="<?php echo URLROOT; ?>home">Home</a>
@@ -44,57 +44,60 @@
         <p><small>ENTERPRISES</small></p>
       </span>
     </a>
-    <a class="toggle-sm" role="button" onclick="dashbC.toggleMenu()">
+    <a class="toggle-sm" role="button" onclick="dashbCon.toggleMenu()">
       <i class="fas fa-bars"></i>
     </a>
   </div>
   <div class="aside-menu-wrap" id="aside_menu_wrap">
     <nav class="aside-menu">
-      <a class="<?php echo ((strpos($data['title'], 'dashboard') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>home">
-        <i class="fas fa-home"></i> <!-- fa-th -->
+      <a class="menu-item <?php echo ((strpos($data['title'], 'dashboard') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>home">
+        <i class="menu-icon fas fa-home"></i> <!-- fa-th -->
         <span>Dashboard</span>
       </a>
       <?php if (isEnabled('catg')) : ?>
-        <a class="<?php echo ((strpos($data['title'], 'category') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>categories">
-          <i class=" fas fa-sitemap"></i> <!-- fa-list-alt -->
+        <a class="menu-item <?php echo ((strpos($data['title'], 'category') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>categories">
+          <i class="menu-icon fas fa-sitemap"></i> <!-- fa-list-alt -->
           <span>Category</span>
         </a>
       <?php endif ?>
       <?php if (isEnabled('brnd')) : ?>
-        <a class="<?php echo ((strpos($data['title'], 'brand') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>brands">
-          <i class="fas fa-tags"></i>
+        <a class="menu-item <?php echo ((strpos($data['title'], 'brand') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>brands">
+          <i class="menu-icon fas fa-tags"></i>
           <span>Brand</span>
         </a>
       <?php endif ?>
       <?php if (isEnabled('atrb')) : ?>
-        <a class="<?php echo ((strpos($data['title'], 'attribute') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>attributes">
-          <i class="fas fa-list"></i> <!-- fa-layer-plus -->
+        <a class="menu-item <?php echo ((strpos($data['title'], 'attribute') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>attributes">
+          <i class="menu-icon fas fa-list"></i> <!-- fa-layer-plus -->
           <span>Attributes</span>
         </a>
       <?php endif ?>
       <?php if (isEnabled('vend')) : ?>
-        <a class="<?php echo ((strpos($data['title'], 'vendor') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>vendors">
-          <i class="fas fa-truck fa-flip-horizontal"></i> <!-- fa-layer-plus -->
+        <a class="menu-item <?php echo ((strpos($data['title'], 'vendor') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>vendors">
+          <i class="menu-icon fas fa-truck fa-flip-horizontal"></i> <!-- fa-layer-plus -->
           <span>Vendor</span>
         </a>
       <?php endif ?>
       <?php if (isEnabled('loca')) : ?>
-        <a class="<?php echo ((strpos($data['title'], 'location') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>locations">
-          <i class="fas fa-map-marker-alt"></i>
+        <a class="menu-item <?php echo ((strpos($data['title'], 'location') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>locations">
+          <i class="menu-icon fas fa-map-marker-alt"></i>
           <span>Location</span>
         </a>
       <?php endif ?>
+
+
       <?php if (isEnabled('prod')) : ?>
-        <a class="<?php echo ((strpos($data['title'], 'product') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>products">
-          <i class="fas fa-box"></i> <!-- fa-cubes-->
+        <a class="menu-item <?php echo ((strpos($data['title'], 'product') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>products">
+          <i class="menu-icon fas fa-box"></i> <!-- fa-cubes-->
           <span>Product</span>
         </a>
       <?php endif ?>
 
-      <div class="drop">
-        <a class="" onclick="showDrop(this);">
-          <i class="fas fa-shopping-cart"></i>
+      <div class="menu-drop">
+        <a class="menu-item">
+          <i class="menu-icon fas fa-shopping-cart"></i>
           <span>Order</span>
+          <i class="drop-icon fas fa-caret-down"></i>
         </a>
         <div class="drop-content">
           <a class="<?php echo ((strpos($data['title'], 'porder') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>purchases">
@@ -105,6 +108,7 @@
           </a>
         </div>
       </div>
+
       <!-- <?php if (isEnabled('ordr')) : ?>
         <a class="<?php echo ((strpos($data['title'], 'order') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>orders">
           <i class="fas fa-shopping-cart"></i>
@@ -112,42 +116,54 @@
         </a>
       <?php endif ?> -->
       <?php if (isEnabled('repo')) : ?>
-        <a class="<?php echo ((strpos($data['title'], 'report') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>reports">
-          <i class="fas fa-chart-area"></i> <!-- fa-analytics -->
+        <a class="menu-item <?php echo ((strpos($data['title'], 'report') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>reports">
+          <i class="menu-icon fas fa-chart-area"></i> <!-- fa-analytics -->
           <span>Report</span>
         </a>
       <?php endif ?>
       <?php if (isEnabled('comp')) : ?>
-        <a class="" href="#">
-          <i class="fas fa-building"></i>
+        <a class="menu-item" href="#">
+          <i class="menu-icon fas fa-building"></i>
           <span>Company</span>
         </a>
       <?php endif ?>
       <?php if (isEnabled('user')) : ?>
-        <a class="<?php echo ((strpos($data['title'], 'user') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>users">
-          <i class="fas fa-users"></i>
+        <a class="menu-item <?php echo ((strpos($data['title'], 'user') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>users">
+          <i class="menu-icon fas fa-users"></i>
           <span>User</span>
         </a>
       <?php endif ?>
       <?php if (isEnabled('role')) : ?>
-        <a class="<?php echo ((strpos($data['title'], 'role') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>roles">
-          <i class="fas fa-user-shield"></i> <!--  fa-shield-alt -->
+        <a class="menu-item <?php echo ((strpos($data['title'], 'role') !== false) ? 'active' : ''); ?>" href="<?php echo URLROOT; ?>roles">
+          <i class="menu-icon fas fa-user-shield"></i> <!--  fa-shield-alt -->
           <span>Role</span>
         </a>
       <?php endif ?>
       <?php if (isEnabled('setn')) : ?>
-        <a class="" href="#">
-          <i class="fas fa-cog"></i>
+        <a class="menu-item" href="#">
+          <i class="menu-icon fas fa-cog"></i>
           <span>Settings</span>
         </a>
       <?php endif ?>
+      <div class="menu-drop">
+        <a class="menu-item">
+          <i class="menu-icon fas fa-cubes"></i>
+          <span>MyMenu</span>
+          <i class="drop-icon fas fa-caret-down"></i>
+        </a>
+        <div class="drop-content">
+          <a class="" href="#">sub_menu_1</a>
+          <a class="" href="#">sub_2</a>
+          <a class="" href="#">menu_hi</a>
+        </div>
+      </div>
     </nav>
     <div class="scroll-menu hide">
-      <a class="" onclick="scrollH.scrollMax();">
+      <a class="" onclick="dashbCon.scrollUp();">
         <i class="fas fa-caret-up"></i>
         <!-- <span>Top</span> -->
       </a>
-      <a class="" onclick="scrollH.scrollMin();">
+      <a class="" onclick="dashbCon.scrollDown();">
         <i class="fas fa-caret-down"></i>
       </a>
     </div>
